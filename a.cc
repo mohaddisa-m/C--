85b396854602e040d464e19a599941c8e8e2c125
Mohaddisa Maryam <mohaddisa321@gmail.com>
2021-01-09 18:28:26 +0500

    Find the qoutient and the remainder.

--- a.cc
+++ a.cc
@@ -1,12 +1,22 @@
 #include <iostream>
 using namespace std;
 
+//enter number1: ___
+//enter number2: ___
+//the quoutient is: __
+//the remainder is: __
+
 int main() {    
-    int number;
-    cout << "Enter a number:";
-    cin >>number;
-    cout<<"your number is :";
-    cout <<number;
+    int number1, number2, quotient, remainder;
+    cout<<"Enter Number1: ";
+    cin>> number1;
+    cout << "Enter number2: ";
+    cin>> number2;
+    quotient = number1 / number2;
+    remainder = number1 % number2;
+    cout<<"The qoutient is: " << quotient<<endl ;
+    cout<< "The remainder is: " << remainder << endl; 
+    
     cout << endl << endl;
-    return 0;
+    return 0 ;
 }
