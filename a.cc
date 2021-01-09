25e9cbdcbe1b8088ee014571351a7406aa9ff5d6
Mohaddisa Maryam <mohaddisa321@gmail.com>
2021-01-09 19:18:10 +0500

    check divisibility

--- a.cc
+++ a.cc
@@ -1,17 +1,24 @@
 #include <iostream>
 using namespace std;
 
+//i%3
+//i%3 ==0 && i%5==0
 int main() {   
-    int m ; 
+   int i; 
     cout << " enter your number ";   
-    cin >> m; 
-    if (m<20){
-        cout << "Number is less than 20";
-    } 
-    else if (m>20) {
-        cout << "Number is greater than 20";
+    cin >> i;
+    if( i%3 ==0 && i%5 ==0) {
+       cout << "divisible by both" << endl;  
     }
-
+     else if (i%3==0){
+        cout << "number is divisible by three" << endl;
+    } 
+      else if (i%5==0) {
+        cout << "Number is divisible by 5";
+    } 
+    else {
+        cout << "not divisible by 3 or 5 " << endl; 
+    }
     cout << endl << endl;
     return 0 ;
 }
