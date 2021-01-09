3a30ac36fc1f75b6dd6927e0262121ea1f9fc6af
Mohaddisa Maryam <mohaddisa321@gmail.com>
2021-01-09 17:39:47 +0500

    cout your number

--- a.cc
+++ a.cc
@@ -2,19 +2,11 @@
 using namespace std;
 
 int main() {    
-    float n1, n2, n3;
-
-    cout << "Enter three numbers: ";
-    cin >> n1 >> n2 >> n3;
-
-    if(n1 >= n2 && n1 >= n3)
-        cout << "Largest number: " << n1;
-
-    if(n2 >= n1 && n2 >= n3)
-        cout << "Largest number: " << n2;
-    
-    if(n3 >= n1 && n3 >= n2)
-        cout << "Largest number: " << n3;
-  
+    int number;
+    cout << "Enter a number:";
+    cin >>number;
+    cout<<"your number is :";
+    cout <<number;
+    cout << endl << endl;
     return 0;
 }
