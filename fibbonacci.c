cdda7fbd5da3009285f87b73c685019057bd4883
Mohaddisa Maryam <mohaddisa321@gmail.com>
2021-01-14 22:48:47 +0500

    2 table to i million

--- fibbonacci.c
+++ fibbonacci.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 
 int main() { 
-    int a,b, product; 
-    printf("enter two numbers ; ");
-    scanf("%d %d" , &a,&b);
-    product = a*b;
-    printf("product is : %d \n\n", product);
+    int i ;
 
+    for ( i = 1; i < 1000001; i++)
+    {
+        printf( "2 x %d = %d\n", i , i*2 );
+    }
+    
+    printf("\n");
     return 0;
+
+
+
 }
+/*
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+*/
