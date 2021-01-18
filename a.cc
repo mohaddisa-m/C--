5b32760647d96dc3526729f90a8fe009db17d2b3
Mohaddisa Maryam <mohaddisa321@gmail.com>
2021-01-18 22:26:41 +0500

    project euler problem 1

--- a.cc
+++ a.cc
@@ -1,22 +1,17 @@
 #include <iostream>
 using namespace std;
 
-// 3 inpits
-// smaller, larger 
-
-
-int main() {
-    int x, y , z ;
-    cout << "Enter three numbers : ";
-    cin >> x >> y >> z ; 
-    if (x > y)
-        swap (x, y); 
-    if (y > z)
-        swap (y,z);   
-    if (y > x)
-        swap (y,x);  
-    if (x > z)
-        swap (x , z);  
-    cout << y << ", " << x << ' ' << z <<  endl ;
-    return 0 ;
+int main(){
+    int i = 0;
+    int sum = 0;
+    for (int i = 1; i < 1000; i++) {
+        if (i%3 == 0){
+                sum +=i;
+        }
+        else if (i%5 == 0){
+                sum +=i; 
+        }
+    }
+    cout<< sum << endl;
+    return 0;
 }
