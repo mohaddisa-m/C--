596689780967c07815759e13995f67460bbfd057
Mohaddisa Maryam <mohaddisa321@gmail.com>
2021-01-22 23:35:59 +0500

    PE Problem 3

--- a.cc
+++ a.cc
@@ -1,17 +1,16 @@
 #include <iostream>
 using namespace std;
-
 int main(){
-    int i = 0;
-    int sum = 0;
-    for (int i = 1; i < 1000; i++) {
-        if (i%3 == 0){
-                sum +=i;
+    unsigned long number = 600851475143;
+    unsigned long product = 1;
+    for (int i = 1; i < 10000; i++) {
+        if (number%i==0) {
+            product *= i;
+            cout << i << endl;
         }
-        else if (i%5 == 0){
-                sum +=i; 
+        if (product>number){
+            break;
         }
     }
-    cout<< sum << endl;
     return 0;
 }
