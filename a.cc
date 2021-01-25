1b66ff594ebde51c28d893c8f1960de3205ae259
Mohaddisa Maryam <mohaddisa321@gmail.com>
2021-01-25 23:44:27 +0500

    parse number into digits

--- a.cc
+++ a.cc
@@ -1,16 +1,15 @@
 #include <iostream>
 using namespace std;
 int main(){
-    unsigned long number = 600851475143;
-    unsigned long product = 1;
-    for (int i = 1; i < 10000; i++) {
-        if (number%i==0) {
-            product *= i;
-            cout << i << endl;
-        }
-        if (product>number){
-            break;
-        }
+    int number;
+    cin >> number; 
+    while (number > 0)
+    {
+        int digit = number % 10;
+        number /= 10; 
+        cout << digit << endl;
     }
+    
+    
     return 0;
 }
