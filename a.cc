31b47259101c2039452e591e9b94154c670d87b2
Mohaddisa Maryam <mohaddisa321@gmail.com>
2021-02-12 16:23:42 +0500

    project tueler problem 7

--- a.cc
+++ a.cc
@@ -1,13 +1,27 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int number;
-    cin >> number; 
-    while (number > 0)
+    int fwd, bwd;
+    bool prime = false;
+    int count=0;
+    for (int fwd = 2; fwd <= 110000; fwd++)
     {
-        int digit = number % 10;
-        number /= 10; 
-        cout << digit << endl;
+        for (int bwd = (fwd-1); bwd > 0; bwd--)
+        {
+            if (fwd%bwd == 0 && bwd !=1){
+                prime = false;
+                break;
+            }
+            if (fwd%bwd == 0 && bwd == 1){
+                prime = true;
+            }
+        }
+        if (prime == true){
+            count +=1;
+            cout << fwd << " is "<< count<< " prime" << endl;
+        }
+        
+        
     }
     
     
